91cfd78072e21743b544ceb914723d2ab7fdacee
Dream Wang <13373237018@163.com>
2017-05-09 11:09:03 +0800

    first commit for starting
    启动页面

---
+++ app/src/main/java/com/wzc/mobileplayer/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.wzc.mobileplayer;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
