8b7786f652c386330b30a49195126a4056b75fb6
Dream Wang <13373237018@163.com>
2017-05-10 16:44:50 +0800

    MainActivity主页面布局
    1.RadioGroup及RadioButton布局
    2.RadioButton相同样式总结、提取、引用
    3.图片选中与未选中的不同状态

--- app/src/main/java/com/wzc/mobileplayer/MainActivity.java
+++ app/src/main/java/com/wzc/mobileplayer/MainActivity.java
@@ -13,13 +13,13 @@ public class MainActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView textView = new TextView(this);
-        textView.setText("我是主页面");
-        textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
+//        TextView textView = new TextView(this);
+//        textView.setText("我是主页面");
+//        textView.setTextSize(30);
+//        textView.setGravity(Gravity.CENTER);
+//        textView.setTextColor(Color.RED);
 
-        setContentView(textView);
+        setContentView(R.layout.activity_main);
 
     }
 }
