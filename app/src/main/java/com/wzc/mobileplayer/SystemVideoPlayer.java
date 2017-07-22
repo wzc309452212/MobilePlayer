996955d8fb3d1bc0ea218921839be93c9d0e6369
Dream Wang <13373237018@163.com>
2017-07-22 22:08:25 +0800

    自定义控制面板下面布局
    1、视频seekbar
    2、一系列的按钮工具
    注意使用findView()自动自成网址

--- app/src/main/java/com/wzc/mobileplayer/SystemVideoPlayer.java
+++ app/src/main/java/com/wzc/mobileplayer/SystemVideoPlayer.java
@@ -9,6 +9,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -16,16 +22,32 @@ import android.widget.VideoView;
  * Created by Administrator on 2017/5/24.
  */
 
-public class SystemVideoPlayer extends Activity {
+public class SystemVideoPlayer extends Activity implements View.OnClickListener {
     // 重写方法 去注册（新版本AS去哪注册啊）
 
     private static final String TAG = SystemVideoPlayer.class.getSimpleName(); // "SystemVideoPlayerActivity"
 
-
-
-
     private VideoView videoview;
     private Uri uri;
+
+    // Content View Elements
+
+    private LinearLayout ll_top;
+    private TextView tv_name;
+    private ImageView iv_battery;
+    private TextView tv_systemtime;
+    private Button btn_voice;
+    private SeekBar seekbar_voice;
+    private Button bt_switch_player;
+    private LinearLayout ll_bottom;
+    private TextView tv_currenttime;
+    private SeekBar seek_video;
+    private TextView tv_duration;
+    private Button bt_exit;
+    private Button bt_pre;
+    private Button bt_start_pause;
+    private Button bt_next;
+    private Button bt_switch_screen;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,14 +57,14 @@ public class SystemVideoPlayer extends Activity {
 
         setContentView(R.layout.activity_system_video_player);
 
-        videoview = (VideoView) findViewById(R.id.videoview);
-
+        findViews();
         setListener();
         getData();
         setData();
 
         // 设置控制面板
-        videoview.setMediaController(new android.widget.MediaController(this));
+       // 第二次 我把你注释掉了 因为我已经写好了自己播放器的控制面板 嘿嘿 不用你了
+        // videoview.setMediaController(new android.widget.MediaController(this));
     }
 
     private void getData() {
@@ -65,6 +87,25 @@ public class SystemVideoPlayer extends Activity {
 
         // 播放完成了的监听
         videoview.setOnCompletionListener(new MyOnCompletionListener());
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v== btn_voice){
+
+        } else if (v== bt_switch_player){
+
+        } else if (v== bt_exit){
+
+        } else if (v== bt_pre){
+
+        } else if (v== bt_start_pause){
+
+        } else if (v== bt_next){
+
+        } else if (v== bt_switch_screen){
+
+        }
     }
 
     private class MyOnPreparedListener implements MediaPlayer.OnPreparedListener {
@@ -135,4 +176,27 @@ public class SystemVideoPlayer extends Activity {
 
         return super.onTouchEvent(event);
     }
+
+    private void findViews() {
+
+        videoview = (VideoView) findViewById(R.id.videoview);
+
+        ll_top = (LinearLayout) findViewById(R.id.ll_top);
+        tv_name = (TextView) findViewById(R.id.tv_name);
+        iv_battery = (ImageView) findViewById(R.id.iv_battery);
+        tv_systemtime = (TextView) findViewById(R.id.tv_systemtime);
+        btn_voice = (Button) findViewById(R.id.btn_voice);
+        seekbar_voice = (SeekBar) findViewById(R.id.seekbar_voice);
+        bt_switch_player = (Button) findViewById(R.id.bt_switch_player);
+        ll_bottom = (LinearLayout) findViewById(R.id.ll_bottom);
+        tv_currenttime = (TextView) findViewById(R.id.tv_currenttime);
+        seek_video = (SeekBar) findViewById(R.id.seek_video);
+        tv_duration = (TextView) findViewById(R.id.tv_duration);
+        bt_exit = (Button) findViewById(R.id.bt_exit);
+        bt_pre = (Button) findViewById(R.id.bt_pre);
+        bt_start_pause = (Button) findViewById(R.id.bt_start_pause);
+        bt_next = (Button) findViewById(R.id.bt_next);
+        bt_switch_screen = (Button) findViewById(R.id.bt_switch_screen);
+    }
+
 }
